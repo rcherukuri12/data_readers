1a06c9ff02cfec430ffcc11dca78a5003593c901
rcherukuri12 <raja_cherukuri@hotmail.com>
2020-05-26 17:42:25 -0700

    adding svm light building method...

--- Sources/readers/DenseInputsProtocol.swift
+++ Sources/readers/DenseInputsProtocol.swift
@@ -1,4 +1,6 @@
 import Foundation
+
 protocol DenseInputsProtocol {
     mutating func load_data(label:String)-> (X:[[Float]],yi:[Int])
+
 }
---
+++ Sources/readers/svm_light_reader.swift
@@ -0,0 +1,13 @@
+// will read libsvm datasets
+import Foundation
+struct svm_light_reader : DenseInputsProtocol{
+    init(filePath:String) {
+    func load_data(label:String)-> (X:[[Float]],yi:[Int]) {
+    var X:[[Float]]=[[]]
+    var y:[String] = []
+
+
+        return (X:[[0.0]],yi:[0])
+    }
+
+}
