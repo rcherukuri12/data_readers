6419da2f6f3495120f387634116736a1da6e040f
rcherukuri12 <raja_cherukuri@hotmail.com>
2020-06-15 08:39:25 -0700

    fixed

--- Sources/readers/svm_light_reader.swift
+++ Sources/readers/svm_light_reader.swift
@@ -1,12 +1,15 @@
 // will read libsvm light datasets
 import Foundation
-struct svm_light_reader : DenseInputsProtocol{
-    init(filePath:String) {
-    func load_data(label:String)-> (X:[[Float]],yi:[Int]) {
-    var X:[[Float]]=[[]]
-    var y:[String] = []
+public struct svm_light_reader : DenseInputsProtocol {
+public init(filePath:String) {
+        self.filePath = filePath
+    }
+    var filePath  :String
 
-
+    public mutating func load_data(label:String)-> (X:[[Float]],yi:[Int]) {
+        var X:[[Float]]=[[]]
+        var yi:[Int] = []
+        // do something to fix things here.
         return (X:[[0.0]],yi:[0])
     }
 
